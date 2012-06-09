5e18b05ce138b440916f67a13b1bb8013a01944c
Aliaksandr Zhuhrou <zhygrr@gmail.com>
2012-06-09 13:55:32 +0300

    savepoint before migrating to mac os

--- ServerLib.dart
+++ ServerLib.dart
@@ -1,9 +1,10 @@
 #library("hypcomm:site");
-#import("../crimson/core/CrimsonLib.dart");
-#import("../crimson/handlers/HandlersLib.dart");
-#import("../log4dart/Lib.dart");
 #import("dart:json");
 #import("dart:io");
+#import("package:CrimsonHttp/core/CrimsonLib.dart");
+#import("package:CrimsonHttp/handlers/HandlersLib.dart");
+#import("package:log4dart/Lib.dart");
+#import("package:MustacheDartPort/Lib.dart");
 #source("server.dart");
 #source("controllers/restController.dart");
 #source("controllers/homeController.dart");
